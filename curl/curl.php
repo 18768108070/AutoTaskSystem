50c12208ad26d70d38342f90f0ae0988c5898361
carl.huang <284642743@qq.com>
2014-06-10 13:03:31 +0800

    项目框架

---
+++ curl/curl.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carl.huang
+ * Date: 14-6-10
+ * Time: 下午12:53
+ */
+
+
+function initCookie(){
+    //TODO 抓取login页面,初始化cookie
+}
+
+function captcha(){
+    //TODO 抓取验证码,保存
+}
+
+function parseCaptchaByOrc(){
+    //TODO King Orc 验证码识别
+}
+
+function login(){
+//TODO POST登陆
+}
+
+function index(){
+//TODO 抓取index页面数据
+}
+
+function initTask(){
+//TODO 抓取Task页面数据
+}
+
+function task(){
+//TODO POST进行下一次任务
+}
