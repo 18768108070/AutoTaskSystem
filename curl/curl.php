f842db0f28888e4b333646dc245ae8ae9ab219a4
carl.huang <284642743@qq.com>
2014-06-10 16:53:10 +0800

    项目框架

--- curl/curl.php
+++ curl/curl.php
@@ -5,32 +5,57 @@
  * Date: 14-6-10
  * Time: 下午12:53
  */
+$f = fopen('1.txt','w');
+fclose($f);
+$cookie = realpath('1.txt');
+echo initCookie($cookie);
 
+function initCookie($cookie)
+{
 
-function initCookie(){
     //TODO 抓取login页面,初始化cookie
+    $ch = curl_init();
+    $header =array('Host:libertagia.com');
+    $options = array();
+    $options[CURLOPT_URL] = 'http://libertagia.com/office/members/login';
+    $options[CURLOPT_USERAGENT] = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36';
+    $options[CURLOPT_FOLLOWLOCATION] = false; //禁止重定向跟随
+    $options[CURLOPT_RETURNTRANSFER] = true; //禁止自动输出
+    $options[CURLOPT_COOKIEJAR] = $cookie;
+    $options[CURLOPT_COOKIEFILE] = $cookie;
+    $options[CURLOPT_HTTPHEADER] = $header;
+    $options[CURLOPT_HEADER] = true; // 输出头信息
+    curl_setopt_array($ch, $options);
+    $result = curl_exec($ch);
+    return $result;
 }
 
-function captcha(){
+function captcha()
+{
     //TODO 抓取验证码,保存
 }
 
-function parseCaptchaByOrc(){
+function parseCaptchaByOrc()
+{
     //TODO King Orc 验证码识别
 }
 
-function login(){
+function login()
+{
 //TODO POST登陆
 }
 
-function index(){
+function index()
+{
 //TODO 抓取index页面数据
 }
 
-function initTask(){
+function initTask()
+{
 //TODO 抓取Task页面数据
 }
 
-function task(){
+function task()
+{
 //TODO POST进行下一次任务
 }
--- utils/parseHtml.php
+++ utils/parseHtml.php
@@ -4,4 +4,13 @@
  * User: carl.huang
  * Date: 14-6-10
  * Time: 下午1:03
- */ 
+ */
+
+
+$ch = curl_init();
+$options = array();
+$options[CURLOPT_URL]='http://www.baidu.com/sdafasdf';
+$options[CURLOPT_FOLLOWLOCATION] = false;
+$options[CURLOPT_HEADER] = true;
+curl_setopt_array($ch,$options);
+$result = curl_exec($ch);
